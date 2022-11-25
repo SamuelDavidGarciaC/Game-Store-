8bc6ebf6b231489dd5587b078b9236504a3549d6
SamuelDavidGarciaC <sd2882418@gmail.com>
2022-11-24 23:05:47 -0500

    antepenúltima-version-game-store

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+
+$correo = $_POST['correo'];
+$nombre = $_POST['nombre'];
+$nombre = $_POST['nombre2'];
+$mensaje = $_POST['mensaje'];
+
+$destinatario = "jefersonmesa13@gmail.com";
+$asunto = "Envio de correo de prueba con PHP";
+$cuerpo = '
+    <html>
+        <head>
+            <title>Prueba de envio de correo</title>
+        </head>
+
+        <body>
+            <h1>Solicitud de contacto desde correo de prueba</h1>
+            <p>
+                Contacto: '.$nombre . ' - ' . $nombre2 .' <br>
+                mensaje: '.$mensaje.'
+            </p>
+        </body>
+    </html>
+
+';
+
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Contect-type: text/html; charset=UTF8\r\n";
+
+$headers .="FROM: $nombre <$correo>\r\n";
+mail($destinatario, $asunto, $cuerpo, $headers);
+
+echo "Correo Enviado";
+?>
+
+<a href="index.html">Volver a Inicio</a>
